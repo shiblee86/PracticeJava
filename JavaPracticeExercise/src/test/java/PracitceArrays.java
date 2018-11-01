72c411ee8b2da0d456bae60a9b34cf681f87cea7
shiblee86 <syed.r.abdullah@gmail.com>
2018-11-01 16:02:45 -0400

    Arrays

---
+++ JavaPracticeExercise/src/test/java/PracitceArrays.java
@@ -0,0 +1,20 @@
+
+public class PracitceArrays {
+
+		public static void main(String[] args) {
+			
+			String[][] credentials =new String[5][2];
+			credentials[0][0]="test2";
+			credentials[0][1]="Joe";
+			credentials[1][0]="Abdullah";
+			credentials[1][1]="BB";
+			credentials[2][0]="fdfdfsdfsdfdf";
+			credentials[3][0]="sdfsdfdsf";
+			credentials[3][1]="fsdfsdf";
+			credentials[4][0]="erwrwerwer";
+			credentials[4][1]="j,jh,j,jh,j,";
+			
+			System.out.println(credentials[2][0]);
+	}
+
+}
