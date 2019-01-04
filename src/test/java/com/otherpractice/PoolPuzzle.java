7eac74b3876abe8a64946e8855b308c09fc0af3a
shiblee86 <syed.r.abdullah@gmail.com>
2019-01-04 14:07:19 -0500

    PoolPuzzle

--- src/test/java/com/otherpractice/PoolPuzzle.java
+++ src/test/java/com/otherpractice/PoolPuzzle.java
@@ -1,5 +1,37 @@
 package com.otherpractice;
 
-public class PoolPuzzle {
+class PoolPuzzle {
+
+	public static void main(String[] args) {
+
+		int x = 0;
+
+		while (x < 4) {
+
+			System.out.print("a");
+
+			if (x < 1) {
+
+				System.out.print(" ");
+			}
+			System.out.println("n");
+
+			if (x > 1) {
+				System.out.print("osyter");
+				x = x + 2;
+			}
+
+			if (x == 1) {
+				System.out.print("noys");
+			}
+			if (x < 1) {
+				System.out.print("oise");
+			}
+
+			System.out.println("");
+
+			x = x + 1;
+		}
+	}
 
 }
